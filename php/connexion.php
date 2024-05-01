dcdc6954dba892410fdccd4293a48b63171c5eac
ServerWeaver <emilien daix@gmail.com>
2024-05-01 19:27:52 +0200

    change title

--- php/connexion.php
+++ php/connexion.php
@@ -45,7 +45,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>inscription | kpay</title>
+    <title>connexion | kpay</title>
     <link rel="icon" href="../image/K.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/inscription.css">
 </head>
